2b4d92983cb675ecf21b1f0af3b7aa504240267f
MorcilloA <35533847+MorcilloA@users.noreply.github.com>
2019-12-10 23:09:56 +0100

    find a card

--- Pokedeck/Deck.java
+++ Pokedeck/Deck.java
@@ -33,4 +33,33 @@ public class Deck {
         return true;
     }
 
+    public void findCard(Deck deck) {
+        
+        Scanner inputZone = new Scanner(System.in);
+        System.out.println("Enter the name of the searched card : ");
+        String name = inputZone.next();
+        System.out.println("Enter the type of the searched card : ");
+        String type = inputZone.next();
+
+        Boolean found = false;
+
+        for (int i = 0; i < deck.getDeck().size(); i++) {
+            String compareName = deck.getDeck().get(i).getName();
+            String compareType = deck.getDeck().get(i).getType();
+            if(compareName == name && compareType == type) {
+
+                System.out.println("test");
+                // System.out.println(deck.getDeck().get(i).getName());
+                found = true;
+                break;
+            }
+            
+        }
+
+        if (!found) {
+            System.out.println("Impossible to find your card");
+        }
+
+    }
+
 }
--- Pokedeck/Pokedeck.java
+++ Pokedeck/Pokedeck.java
@@ -67,7 +67,7 @@ public class Pokedeck {
                     break;
 
                 case "findCard":
-                    
+                    myDeck.findCard(myDeck);
                     break;
 
                 case "addCard":
