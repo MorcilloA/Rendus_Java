c521e934eccb7e5720931f32e604c924ee8c99d0
MorcilloA <35533847+MorcilloA@users.noreply.github.com>
2019-12-10 17:37:06 +0100

    Add files via upload
    add choosing action system

--- Pokedeck/Deck.java
+++ Pokedeck/Deck.java
@@ -6,17 +6,27 @@ import java.util.*;
 public class Deck {
 
     private ArrayList<Card> deck;
+    private static int cpt = 1;
 
     public Deck() {
         this.deck = new ArrayList<Card>();
+        Deck.cpt++;
     }
 
     public ArrayList<Card> getDeck() {
         return this.deck;
     }
 
+    public static int getCpt() {
+        return Deck.cpt;
+    }
+
     public Boolean addCard(Card newCard) {
-        return true;
+        if(deck.add(newCard)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public Boolean removeCard(String pokemonName, String pokemonType) {
--- Pokedeck/Pokedeck.java
+++ Pokedeck/Pokedeck.java
@@ -1,18 +1,71 @@
 /**
  * Pokedeck
  */
+import java.util.*;
+
 public class Pokedeck {
 
     public static void main(String[] args) {
         // System.out.println("test");
 
+        ArrayList<String> actions = new ArrayList<>();
+        actions.add("choose");
+        actions.add("createDeck");
+        actions.add("addCard");
+        actions.add("stopProgram");
+
         Deck myDeck = new Deck();
 
-        Card newCard = new Card();
+        Boolean running = true;
+
+        while (running) {
+            
+            System.out.println("Choose your action from the list :");
+            //Define the input zone
+            
+            Scanner inputZone = new Scanner(System.in);
+            
+            for (int i = 1; i < actions.size(); i++) {
+                System.out.println(actions.get(i));
+            }
+            String action = inputZone.next();
+            
+            while (!actions.contains(action)) {
+                System.out.println("Action undefined please select an action from the list below :");
+                for (int i = 0; i < actions.size(); i++) {
+                    System.out.println(actions.get(i));
+                }
+                action = inputZone.next();
+            }
+
+            switch (action) {
+                // case "createDeck":
+                    // Deck myNewDeck = new Deck();
+                    // break;
+
+                case "addCard":
+                    if (Deck.getCpt() != 0) {
+                        Card newCard = new Card();
+                        myDeck.addCard(newCard);
+                    }
+                    break;
+
+                case "stopProgram":
+                    System.out.println("Program stopped");
+                    running = false;
+                    break;
+            
+                default:
+                    running = true;
+                    break;
+            }
+
+
         
-        
+            // System.out.println(myDeck.getDeck().get(0).getName());
+        }
 
 
 
     }
-}
+}   
