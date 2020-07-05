67b8b376a1598073ec96c4b06eae52283b2d2f7d
MorcilloA <alexy.morcillo@gmail.com>
2020-07-05 16:35:55 +0100

    settings first try

--- Pomodoro/components/Settings.js
+++ Pomodoro/components/Settings.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
+import ReactDOM from "react-dom";
 
-export default function Settings() {
+const rootElement = document.getElementById("root");
+
+// export default function Settings() {
+function Settings() {
+
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }; // your form submit function which will invoke after successful validation
+
+    console.log(watch("example")); // you can watch individual input by pass the name of the input
+
     return (
-        <View>
-            <Text>Page Settings</Text>
-        </View>
-    )
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Example</label>
+            <input name="example" defaultValue="test" ref={register} />
+            <label>ExampleRequired</label>
+            <input
+                name="exampleRequired"
+                ref={register({ required: true, maxLength: 10 })}
+            />
+            {errors.exampleRequired && <p>This field is required</p>}
+            <input type="submit" />
+        </form>
+    );
 }
+
+export default ReactDOM.render(<Settings />, rootElement);
